7241201503f05a79dfa4b6b610e30fc83be0227c
Lukas Tomasek <vedstav31@gmail.com>
2024-03-23 19:22:02 -0400

    code example with template literals and array

--- javascript/index.js
+++ javascript/index.js
@@ -1,99 +1,141 @@
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container")
-const addTaskButton = document.getElementById('add-task-btn')
-const ADD_TASK_BTN = "add-task-btn"
-const EDIT_BTN_ID = "edit-btn-id"
-const COMPLETE_BTN_ID = "complete-btn-id"
-const DELETE_BTN_ID = "delete-btn-id"
-const TODO_ITEM_ID = "todo-item-id"
-const CHECK_INPUT_ID = "check-input-id"
+/*
+1. Add Task
+2. Edit Task
+3. Complete Task
+4. Remove Task
+*/
+
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
+const addTaskButton = document.getElementById('add-task-btn');
+const ADD_TASK_BTN = 'add-task-btn';
+const EDIT_BTN_ID = 'edit-btn-id';
+const COMPLETE_BTN_ID = 'complete-btn-id';
+const DELETE_BTN_ID = 'delete-btn-id';
+const TODO_ITEM_ID = 'todo-item-id';
+const CHECK_INPUT_ID = 'check-input-id';
 const todoItems = [];
+let index = 0;
+const test = 'test';
 
-window.onload = function() {
-    loadTasks()
-  }
+window.onload = function () {
+ loadTasks();
+};
 
-addTaskButton.addEventListener('click', addTask)
+addTaskButton.addEventListener('click', addTask);
 
-window.addEventListener('click', function(e) {
-  const id = e.target.id;
+window.addEventListener('click', function (e) {
+ const id = e.target.id;
 
-  switch(id) { 
-    case EDIT_BTN_ID:
-        editTask(e)
-      break;
-    case CHECK_INPUT_ID:
-        completeTask(e)
-      break;
-    case DELETE_BTN_ID:
-        removeTask(e)
-      break;
-  }
-  
-  saveTasks()
-})
+ switch (id) {
+  case EDIT_BTN_ID:
+   editTask(e);
+   break;
+  case CHECK_INPUT_ID:
+   completeTask(e);
+   break;
+  case DELETE_BTN_ID:
+   removeTask(e);
+   break;
+ }
 
+ saveTasks();
+});
 
-function addTask() {  
-    if (inputBox.value === "") {
-        return;
-      }  
-      const item = document.createElement("li");
-      item.innerHTML = inputBox.value;
-      listContainer.appendChild(item);
-      item.setAttribute("id", "todo-item-id");
-      item.setAttribute("class", "animate-left");
+function addTask() {
+ if (inputBox.value === '') {
+  return;
+ }
 
-      const checkInput = document.createElement("input")
-      checkInput.type = "checkbox"
-      listContainer.appendChild(checkInput)
-      checkInput.setAttribute("class", "checkbox-input")
-      checkInput.setAttribute("id", "check-input-id")
-      checkInput.classList.add("animate-opacity-delay");
+ index++;
 
-      const editButton = document.createElement("div");
-      editButton.innerHTML = "&#9998;";
-      listContainer.appendChild(editButton);
-      editButton.setAttribute("id", "edit-btn-id");
-      editButton.setAttribute("class", "edit-button");
-      editButton.classList.add("animate-opacity");
+ const parentElement = document.createElement('li');
+ parentElement.setAttribute('data-index', index);
+ parentElement.innerHTML = createTodo(inputBox.value);
 
-      const deleteButton = document.createElement("span");
-      deleteButton.innerHTML = "\u00d7";
-      listContainer.appendChild(deleteButton);
-      deleteButton.classList.add("animate-opacity");
-      deleteButton.setAttribute("id", "delete-btn-id")
+ listContainer.appendChild(parentElement);
 
-    saveTasks()
-    inputBox.value = "";
-  }
-  console.log(todoItems)
+ /*
+ const item = document.createElement('li');
+ item.innerHTML = inputBox.value;
+ listContainer.appendChild(item);
+ item.setAttribute('id', 'todo-item-id');
+ item.setAttribute('class', 'animate-left');
+ item.setAttribute('data-index', index);
 
-  let toggleText = false;
+ const checkInput = document.createElement('input');
+ checkInput.type = 'checkbox';
+ item.appendChild(checkInput);
+ checkInput.setAttribute('class', 'checkbox-input');
+ checkInput.setAttribute('id', 'check-input-id');
+ checkInput.classList.add('animate-opacity-delay');
+
+ const editButton = document.createElement('div');
+ editButton.innerHTML = '&#9998;';
+ item.appendChild(editButton);
+ editButton.setAttribute('id', 'edit-btn-id');
+ editButton.setAttribute('class', 'edit-button');
+ editButton.classList.add('animate-opacity');
+
+ const deleteButton = document.createElement('span');
+ deleteButton.innerHTML = '\u00d7';
+ item.appendChild(deleteButton);
+ deleteButton.classList.add('animate-opacity');
+ deleteButton.setAttribute('id', 'delete-btn-id');
+ */
+
+ saveTasks();
+ inputBox.value = '';
+
+ todoItems.push({
+  element: parentElement,
+  id: String(index),
+ });
+ console.log(typeof index);
+ console.log(typeof parentElement.getAttribute('data-index'));
+}
+
+let toggleText = false;
 
 function editTask(e) {
-        toggleText = !toggleText;
-        e.target.contentEditable = false
-        e.target.previousElementSibling.previousElementSibling.contentEditable = toggleText;
+ toggleText = !toggleText;
+ const todo = getTodoItem(e.target.parentElement.getAttribute('data-index'));
+ todo.element.contentEditable = toggleText;
 }
 
 function completeTask(e) {
-  e.target.previousElementSibling.classList.toggle("checked")
+ const todo = getTodoItem(e.target.parentElement.getAttribute('data-index'));
+ todo.element.classList.toggle('checked');
 }
 
 function removeTask(e) {
-      e.target.previousElementSibling.previousElementSibling.previousElementSibling.remove();
-       e.target.previousElementSibling.previousElementSibling.remove();
-        e.target.previousElementSibling.remove();
-        e.target.remove();
-} 
+ const todo = getTodoItem(e.target.parentElement.getAttribute('data-index'));
+ console.log(typeof todo.id);
+ todo.element.remove();
+ todoItems.splice(todoItems.indexOf(todo), 1);
+}
+
+function getTodoItem(id) {
+ const todo = todoItems.find((el) => el.id === id);
+
+ return todo;
+}
+
+function createTodo(text) {
+ return ` <p>${text}</p>
+          <div">
+            <button>edit</button>
+            <button>remove</button>
+          </div>
+    `;
+}
 
 function saveTasks() {
-    localStorage.setItem("data", listContainer.innerHTML);
+ localStorage.setItem('data', listContainer.innerHTML);
 }
 
 function loadTasks() {
-        listContainer.innerHTML = localStorage.getItem("data");
+ listContainer.innerHTML = localStorage.getItem('data');
 }
 
-localStorage.clear()
+localStorage.clear();
